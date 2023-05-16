f74e4ca44e1689caa10ab2d53bcc4c457a1eaccc
pedrohassen <pedro_hasse@yahoo.com.br>
2023-05-15 23:09:28 -0300

    Refactor ProductsController to handle error responses in updatePrice method

--- backend/src/api/controllers/products.controller.ts
+++ backend/src/api/controllers/products.controller.ts
@@ -1,22 +1,17 @@
 import { type Request, type Response } from 'express';
 import type ProductsService from '../services/products.service';
-import csvImporter from '../utils/csv.importer';
 
 export default class ProductsController {
   constructor (private readonly service: ProductsService) { }
 
   public findAll = async (_req: Request, res: Response): Promise<Response> => {
     const products = await this.service.findAll();
-    return res.status(200).json(products);
+    return res.status(200).json({ message: products });
   };
 
   public updatePrice = async (req: Request, res: Response): Promise<Response> => {
-    const file = req.file;
-    if (!file) {
-      return res.status(400).json({ message: 'No file uploaded' });
-    }
-    const updates = await csvImporter(file.path);
-    await this.service.updatePrice(updates);
-    return res.status(204).end();
+    const { error, message } = await this.service.updatePrice(req.body);
+    if (error) return res.status(400).json({ error });
+    return res.status(200).json({ message });
   };
 }
