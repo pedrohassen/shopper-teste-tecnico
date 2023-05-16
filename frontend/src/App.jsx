5fe1dc1d9e551dee60e25e545653fe7c265d65a0
pedrohassen <pedro_hasse@yahoo.com.br>
2023-05-15 23:05:06 -0300

    Add App component with CSVFileInput to handle CSV file input

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CSVFileInput from './Components/CSVFileInput';
+
+function App() {
+  return (
+    <div>
+      <h1>Carregamento de Arquivo CSV</h1>
+      <CSVFileInput />
+    </div>
+  );
+}
+
+export default App;
