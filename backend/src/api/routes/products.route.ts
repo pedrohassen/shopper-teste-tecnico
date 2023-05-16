9e0db76425b6a15d5ceeec5236bac434c356722f
pedrohassen <pedro_hasse@yahoo.com.br>
2023-05-15 23:06:23 -0300

    Add joi validation, file validation and adapt requisition middlewares to the updatePrice method in the Products route

--- backend/src/api/routes/products.route.ts
+++ backend/src/api/routes/products.route.ts
@@ -1,20 +1,20 @@
 import express from 'express';
-import multer from 'multer';
-import os from 'os';
 import ProductsController from '../controllers/products.controller';
 import ProductsService from '../services/products.service';
 import ProductsModel from '../models/products.model';
+import joiValidation from '../middlewares/joi.validation';
+import fileValidation from '../middlewares/file.validation';
+import adapter from '../middlewares/adapt.requisition';
 
 const productsModel = new ProductsModel();
 const productsService = new ProductsService(productsModel);
 const productsController = new ProductsController(productsService);
 
 const router = express.Router();
-const upload = multer({
-  dest: os.tmpdir()
-});
 
 router.get('/', productsController.findAll);
-router.put('/', upload.single('file'), productsController.updatePrice);
+router.put('/', joiValidation(fileValidation),
+  adapter,
+  productsController.updatePrice);
 
 export default router;
