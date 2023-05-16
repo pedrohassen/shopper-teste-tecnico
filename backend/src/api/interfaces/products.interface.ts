81d2610b0261a8c57c0b0aa657f11bbd97a48099
pedrohassen <pedro_hasse@yahoo.com.br>
2023-05-15 23:06:49 -0300

    "Add new interfaces for Products and ResponseReturn in products.interface.ts"

--- backend/src/api/interfaces/products.interface.ts
+++ backend/src/api/interfaces/products.interface.ts
@@ -1,6 +1,20 @@
-export default interface IProducts {
+import { type Prisma } from '@prisma/client';
+
+export interface IProducts {
   code: number;
   name?: string;
   price?: number;
   sales_price: number;
+}
+
+export interface IProductsCsv {
+  code?: number;
+  costValue: Prisma.Decimal;
+  actualPrice: Prisma.Decimal;
+  updatedPrice: Prisma.Decimal;
+}
+
+export interface ResponseReturn {
+  message?: string;
+  error?: string;
 }
